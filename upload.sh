ecfe62f3c1bdb979b0fd7e4c03cdbdd0cf20bc68
yangdw <dwyang@vw-mobvoi.com>
2023-12-27 14:10:13 +0800

    Summary: Update code and version to 1.2.0
    1, update to compatible gradle 8.1 and openjdk 17
    2, update javadoc generation method.
       as openjdk 11 and later version does not support sun api, google
       doclava does not work anymore, so we use gradle task javadoc instead.
    3, update version to 1.2.0
    
    Change-Id: I3e3bbc5e6d307fe18e2a2babd9b54f5f76281a45

---
+++ upload.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+### you should input your module name to execute this action.
+# such as:
+# ./gradlew lib-core:publishMavenJavaPublicationToSonatypeRepository
+
+./gradlew $1:publishMavenJavaPublicationToMavenRepository
